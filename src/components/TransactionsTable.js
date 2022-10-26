9faf9c1eee487b591b0c865416b712cc6ebe0766
Saugat D <saugat.dhungel@gmail.com>
2022-10-26 17:44:40 -0400

    create a table for transactions.

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -20,11 +20,44 @@ const TransactionsTable = () => {
       )
     );
   }
-  console.log(transactions);
+  console.log("tst", transactions);
   useEffect(() => {
     const fetchDataOnLoad = async () => {
       const { data } = await getTransactions();
-      setTransactions(data);
+      const augustTransactions = data.transactions
+        .filter(
+          (a) =>
+            new Date(a.date) - new Date("8/01/2022") > 0 &&
+            new Date(a.date) - new Date("9/01/2022") < 0
+        )
+        .sort((a, b) => {
+          return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
+      const septTransactions = data.transactions
+        .filter(
+          (a) =>
+            new Date(a.date) - new Date("9/01/2022") > 0 &&
+            new Date(a.date) - new Date("10/01/2022") < 0
+        )
+        .sort((a, b) => {
+          return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
+
+      const octTransactions = data.transactions
+        .filter(
+          (a) =>
+            new Date(a.date) - new Date("10/01/2022") > 0 &&
+            new Date(a.date) - new Date("11/01/2022") < 0
+        )
+        .sort((a, b) => {
+          return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
+      setTransactions({
+        ...transactions,
+        august: augustTransactions,
+        september: septTransactions,
+        october: octTransactions,
+      });
     };
     fetchDataOnLoad();
   }, []);
@@ -34,89 +67,64 @@ const TransactionsTable = () => {
       <table>
         <thead>
           <tr className="month-row">
-            <th colSpan="3">August</th>
+            <th colSpan="3">August Transactions</th>
           </tr>
           <tr className="header-row">
             <th>Customer Name</th>
-            <th>Purchases</th>
+            <th>Transaction Date</th>
             <th>Total Spent</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Anom</td>
-            <td>19</td>
-            <td>Male</td>
-          </tr>
-          <tr>
-            <td>Megha</td>
-            <td>19</td>
-            <td>Female</td>
-          </tr>
-          <tr>
-            <td>Subham</td>
-            <td>25</td>
-            <td>Male</td>
-          </tr>
+          {transactions.august.map((transaction) => (
+            <tr>
+              <td>{transaction.customerName}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.total}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <table>
         <thead>
           <tr className="month-row">
-            <th colSpan="3">August</th>
+            <th colSpan="3">September Transactions</th>
           </tr>
           <tr className="header-row">
             <th>Customer Name</th>
-            <th>Purchases</th>
+            <th>Transaction Date</th>
             <th>Total Spent</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Anom</td>
-            <td>19</td>
-            <td>Male</td>
-          </tr>
-          <tr>
-            <td>Megha</td>
-            <td>19</td>
-            <td>Female</td>
-          </tr>
-          <tr>
-            <td>Subham</td>
-            <td>25</td>
-            <td>Male</td>
-          </tr>
+          {transactions.september.map((transaction) => (
+            <tr>
+              <td>{transaction.customerName}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.total}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
-
       <table>
         <thead>
           <tr className="month-row">
-            <th colSpan="3">August</th>
+            <th colSpan="3">October Transactions</th>
           </tr>
           <tr className="header-row">
             <th>Customer Name</th>
-            <th>Purchases</th>
+            <th>Transaction Date</th>
             <th>Total Spent</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Anom</td>
-            <td>19</td>
-            <td>Male</td>
-          </tr>
-          <tr>
-            <td>Megha</td>
-            <td>19</td>
-            <td>Female</td>
-          </tr>
-          <tr>
-            <td>Subham</td>
-            <td>25</td>
-            <td>Male</td>
-          </tr>
+          {transactions.october.map((transaction) => (
+            <tr>
+              <td>{transaction.customerName}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.total}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
